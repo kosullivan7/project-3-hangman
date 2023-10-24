34a06c0de69b662bce996ef4e5822522b2c6c3f9
kosullivan7 <kosullivan77@gmail.com>
2023-10-24 15:54:55 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,32 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Project 3, Hangman
+"""
+
+from time import sleep
+import random
+from hangman_parts import parts
+
+#Prints a hangman picture
+print("Welcome to Hangman")
+print('   ',  '------')
+print('   ',  '|    |')
+print('   ',  '|    O')
+print('   ',  '|   -|-')
+print('   ',  '|    |')
+print('   ',  '|   / \\')
+print('------------')
+    
+
+print(
+        """
+            To play the game you must guess the letters of the hidden word.
+            If the guess is correct, the letter missing in the word
+            is replaced by the correct letter.
+            You can enter the whole word if you know what the word is.
+            Each wrong guess takes one life. You have four lives in total.
+            
+            Good luck!
+            """
+            )
+
+
